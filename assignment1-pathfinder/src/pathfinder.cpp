af630a8acb8cedb57c3a92f7884e2a325016cc25
RKettles <B01741425@studentmail.uws.ac.uk>
2025-02-18 14:09:27 +0000

    Merged Zac and Robbie

--- assignment1-pathfinder/src/pathfinder.cpp
+++ assignment1-pathfinder/src/pathfinder.cpp
@@ -34,12 +34,10 @@ unsigned int path_cost(const std::vector<node_t>& path)
 
 int main()
 {
-    //Screen size
-  const int w{ 2280/2 }, h{ 1620/2 }, half_w{ w/2 }, half_h{ h/2 }, gap{ w/8 };
+  const int w{ 2880/2 }, h{ 1620/2 }, half_w{ w/2 }, half_h{ h/2 }, gap{ w/8 };
   raylib::Window window{ w, h, "Pathfinder" };
 
   SetTargetFPS(60);
-
 
   Graph g;
   add_node(g, 'A', { half_w - gap, half_h });
@@ -49,17 +47,6 @@ int main()
   add_node(g, 'E', { half_w + gap, half_h + gap });
   add_node(g, 'F', { half_w + gap, half_h });
   add_node(g, 'G', { half_w + (2 * gap), half_h - gap });
-
-  //testing nodes
-  //left
-  add_node(g, 'H', { half_w - (2 * gap), half_h });
-  //right
-  add_node(g, 'I', { half_w + (2 * gap), half_h });
-  //down
-  add_node(g, 'J', { half_w, half_h + (2 * gap) });
-  //up
-  add_node(g, 'K', { half_w, half_h - (2 * gap) });
-
   add_double_edge(g, 'A', 'B');
   add_double_edge(g, 'B', 'C');
   add_double_edge(g, 'B', 'D');
@@ -74,9 +61,13 @@ int main()
 
   int t{60}; // time
   std::vector<node_t> player_path{};
-  node_t start = 'A';
-  node_t end   = 'G';
+  //Task 14 - Robbie
+
+  node_t start = 'A' + GetRandomValue(0, 6);
+  node_t end   = 'A' + GetRandomValue(0, 6);
+
   int tokens{2000}, score{}, high_score{}; // try with more/less tokens?
+  int frames = 0;
 
   while (!window.ShouldClose()) // Detect window close button or ESC key
   {
@@ -84,23 +75,28 @@ int main()
 
     ClearBackground(LIGHTGRAY);
 
-    //Numbers are X, Y, font size
-    //Need to find out what type of time their after
-    DrawText(TextFormat("Score: %08i", score), 10, 10, 20, RED);
-    DrawText(TextFormat("Tokens: %08i", tokens), 190, 10, 20, ORANGE);
-    DrawText(TextFormat("High_score: %08i", high_score), 380, 10, 20, PURPLE);
-    DrawText(TextFormat("Timer: %02.02f ms", GetTime() * 1000, t), 610, 10, 20, BLACK);
-    DrawText(TextFormat("T: %08i", t), 780, 10, 20, PURPLE);
-
     draw_graph(g);
+
+    //Task 2 - Robbie
+    DrawCircle(node_info[start].x, node_info[start].y, 10, GREEN);
+    DrawCircle(node_info[end].x, node_info[end].y, 10, RED);
+
+    // Task 12 - Robbie
+    frames++; // increase the frames int by 1 every frame
+    if (frames == 60) {
+        t--; //Decrease time every 60 frames
+        frames = 0;
+        
+    }
+  
+    DrawText(TextFormat("Time: %d", t), 10, 100, 20, BLACK);
+
 
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
     {
       if (auto opt = get_nearby_node(GetMousePosition()))
       {
         // *opt is a node_t
-          DrawText(TextFormat("Score: %08i", score), 110, 110, 20, RED);
-          add_node(g, 'Z', { half_w, half_h });
       }
     }
 
